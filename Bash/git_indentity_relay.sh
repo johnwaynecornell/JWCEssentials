8ce4f29175100ab19654bea56a7347a41a6dbe79
jwc <john.wayne.cornell@gmail.com>
2026-05-09 10:33:04 -0400

    **JWCEssentials** - cleanup git identity relay script

--- Bash/git_indentity_relay.sh
+++ Bash/git_indentity_relay.sh
@@ -4,10 +4,9 @@
 GIT_USER=$(git config user.name)
 GIT_EMAIL=$(git config user.email)
 
-echo "# Run the following commands in your VM to set your Git identity:"
+echo "# Run the following commands in bash to set your Git identity:"
 echo "---------------------------------------------------------------"
-echo "git config --global user.name \"$GIT_USER\""
-echo "git config --global user.email \"$GIT_EMAIL\""
+echo "git config --global user.name \"$GIT_USER\"; git config --global user.email \"$GIT_EMAIL\""
 echo ""
 echo "# Optional: Set a short credential cache timeout (1 hour)"
 echo "git config --global credential.helper 'cache --timeout=3600'"
