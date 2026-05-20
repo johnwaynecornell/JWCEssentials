e4e77bfc24626784c8a716d31c1fb408f84120ac
jwc <john.wayne.cornell@gmail.com>
2026-05-20 12:25:37 -0400

    **NewAge** - silence warning

--- Dev/build_native.sh
+++ Dev/build_native.sh
@@ -85,9 +85,9 @@ build_directory()
   cd "$build_dir"
 
   if [ "$FRESH" = "1" ]; then
-    verbose.sh cmake -S "$source_dir" -B . -DCMAKE_BUILD_TYPE="$NewAge_Config" --fresh
+    verbose.sh cmake -S "$source_dir" -B . -DCMAKE_BUILD_TYPE="$NewAge_Config" --no-warn-unused-cli --fresh
   else
-    verbose.sh cmake -S "$source_dir" -B . -DCMAKE_BUILD_TYPE="$NewAge_Config"
+    verbose.sh cmake -S "$source_dir" -B . -DCMAKE_BUILD_TYPE="$NewAge_Config" --no-warn-unused-cli
   fi
 
   if [ "$CLEAN" = "1" ]; then
