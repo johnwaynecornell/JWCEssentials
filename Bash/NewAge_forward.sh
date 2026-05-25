76d428c8fe89c33755de3d990cf6726f2cce5025
jwc <john.wayne.cornell@gmail.com>
2026-05-25 11:53:46 -0400

    **JWCEssentials** - script add varient

--- Bash/NewAge_forward.sh
+++ Bash/NewAge_forward.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 Project="$1"
-MyReferencePath="$(cygpath "$2")"
-SourceDir="$3"
+MyReferencePath="$(cygpath $2)"
+SourceDir="$(cygpath $3)"
 
 if [ ! -d "$MyReferencePath" ]; then
     mkdir -p "$MyReferencePath"
@@ -16,16 +16,18 @@ fi
 try_link() {
     echo _______________________________
 
-    O="$MyReferencePath/$1"
+    O="$MyReferencePath$1"
     pth="$SourceDir"
     I="$(resolve_path.sh $pth)/$1"
 
     
+    if [ ! -f "$I" ]; then
+        I="$(resolve_path.sh $pth)/$1.exe"
+    fi
+
     echo "O=$O"
     echo "pth=$pth"
     echo "I=$I"
-
-    pwd
 
 
     # Check if the output path already exists as a file or symbolic link and remove it if necessary
@@ -36,17 +38,16 @@ try_link() {
     # Check if the input path exists and create a symbolic link
     if [ -f "$I" ]; then
 	echo input exists
-        verbose.sh create_symlink.sh "$I" "$O"
+        
+        echo "#!/bin/bash" > "$O"
+        echo "\"$I\" \"\$@\"" >> "$O"
+	chmod +110 "$O"
     fi
 
     echo _______________________________
 }
 
 try_link "$Project"
-try_link "$Project.exe"
-try_link "$Project.dll"
-try_link "$Project.pdb"
-try_link "$Project.deps.json"
 
 echo $Project staged
 
